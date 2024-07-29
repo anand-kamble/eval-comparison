ae9e6e80b9ea558db5ba660ba13931fe0c1734d8
anand-kamble <amk23j@fsu.edu>
2024-07-29 10:04:51 -0400

    Added results of the evaluation

--- ragas_evaluation.py
+++ ragas_evaluation.py
@@ -1,3 +1,4 @@
+# %%
 import time
 from ragas.integrations.llama_index import evaluate
 from ragas.metrics.critique import harmfulness
@@ -8,48 +9,67 @@ from ragas.metrics import (
     context_recall,
 
 )
+from dotenv import load_dotenv
 import json
+import os
 from llama_index.core.settings import Settings
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 from llama_index.llms.ollama import Ollama
+from llama_index.llms.openai import OpenAI
 # from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.embeddings.ollama import OllamaEmbedding
 from ragas.testset.evolutions import simple, reasoning, multi_context, conditional
 from llama_index.core import SimpleDirectoryReader
 from datasets import Dataset
+# %%
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
 
+# from openai import OpenAI
+
+# client = OpenAI(
+#   organization='org-fqDYyIhzVe2v3cXvuB1rPfrH',
+#   project='proj_zFvkDFbYgbKKfodcKGHSBPB9',
+# )
+
+
+# %%
 time_dict = {}
 start_time = time.time()
-
+# %%
 # embeddings = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")  # OpenAIEmbedding()
-embeddings = OllamaEmbedding(model_name="phi3:latest")
+embeddings = OllamaEmbedding(model_name="llama3",base_url="http://class02:11434")
 Settings.embed_model = embeddings
 end_time = time.time()
 time_dict['embedding_setup'] = end_time - start_time
 print("Time taken for embedding setup: ", time_dict['embedding_setup'])
-
+# %%
 start_time = time.time()
 documents = SimpleDirectoryReader(
     "./data/EvaluatingLlmSurveyPaperDataset", required_exts=[".pdf"], recursive=True).load_data()
 end_time = time.time()
 time_dict['document_loading'] = end_time - start_time
 print("Time taken for document loading: ", time_dict['document_loading'])
-
+# %%
 print("Building the vector index...")
 start_time = time.time()
 vector_index = VectorStoreIndex.from_documents(documents[:2])
 end_time = time.time()
 time_dict['vector_index_building'] = end_time - start_time
-print("Time taken for vector index building: ", time_dict['vector_index_building'])
-
+print("Time taken for vector index building: ",
+      time_dict['vector_index_building'])
+# %%
 print("Building the query engine...")
 start_time = time.time()
-generator_llm = Ollama(model="phi3:latest",request_timeout=600.0)
+generator_llm = Ollama(model="phi3:latest", request_timeout=600.0,
+                       base_url="http://class02:11434",
+                       additional_kwargs={"max_length": 512})
 query_engine = vector_index.as_query_engine(llm=generator_llm)
 end_time = time.time()
 time_dict['query_engine_building'] = end_time - start_time
-print("Time taken for query engine building: ", time_dict['query_engine_building'])
-
+print("Time taken for query engine building: ",
+      time_dict['query_engine_building'])
+# %%
 metrics = [
     faithfulness,
     answer_relevancy,
@@ -58,12 +78,13 @@ metrics = [
     harmfulness,
     # max_workers = 24 # Found this argument from the RunConfig dataclass.
 ]
-
-critic_llm = Ollama(model="llama3",request_timeout=600.0)  # OpenAI(model="gpt-4")
+# %%
+#   # OpenAI(model="gpt-4")
+critic_llm = OpenAI(model="gpt-4o-mini") #Ollama(model="llama3",base_url="http://class01:11434",request_timeout=600.0)
 # using GPT 3.5, use GPT 4 / 4-turbo for better accuracy
 evaluator_llm = critic_llm  # OpenAI(model="gpt-3.5-turbo")
 # USING CRITIC LLM TO KEEP EVERYTHING LOCAL FOR NOW.
-
+# %%
 
 # For testset needs to be a dictionary with keys "question" and "ground_truth"
 # From which the question and ground_truth are lists of strings.
@@ -79,6 +100,8 @@ evaluator_llm = critic_llm  # OpenAI(model="gpt-3.5-turbo")
 #         "K-means clustering is a partitional clustering approach that divides the data into non-overlapping subsets (clusters), while hierarchical clustering produces a set of nested clusters organized as a hierarchical tree. K-means requires specifying the number of clusters in advance, while hierarchical clustering does not. K-means typically has a global objective function, while hierarchical clustering algorithms have local objectives.",
 #     ]}
 # To make this testset from the llama Dataset, we can convert the dataset to a dictionary as follows:
+
+# %%
 start_time = time.time()
 
 llama_rag_dataset = None
@@ -96,7 +119,6 @@ for item in llama_rag_dataset["examples"][:5]:
     testset["question"].append(item["query"])
     testset["ground_truth"].append(item["reference_answer"])
 
-
     """
     Here I realized that for evluation we need to provide a Dataset object to the ragas evaluate function.
     Not just the dictionary. So I will convert the dictionary to a Dataset object.
@@ -109,7 +131,7 @@ dataset = Dataset.from_dict(testset)
 end_time = time.time()
 time_dict['testset_loading'] = end_time - start_time
 print("Time taken for testset loading: ", time_dict['testset_loading'])
-
+# %%
 
 print("Evaluating the query engine...")
 start_time = time.time()
@@ -118,17 +140,20 @@ result = evaluate(
     metrics=metrics,
     dataset=testset,
     llm=evaluator_llm,
-    embeddings=OllamaEmbedding(model_name="phi3:latest"),
+    embeddings=OllamaEmbedding(model_name="llama3",base_url="http://class03:11434"),
 )
 
 end_time = time.time()
+# %%
 time_dict['evaluation'] = end_time - start_time
 print("Time taken  for evaluation: ", time_dict['evaluation'])
-
+# %%
 result.to_pandas().to_csv("ragas_evaluation.csv")
-
+# %%
 
 # Save timing results to a text file
 with open("timing_results.txt", "w") as f:
     for key, value in time_dict.items():
         f.write(f"{key}: {value} seconds\n")
+
+# %%
