cfa41afbf183199f35fa1d2767e462d8b0137f7b
anand-kamble <amk23j@fsu.edu>
2024-09-20 11:09:53 -0400

    Opik eval tests - failed

--- opik_eval/main.py
+++ opik_eval/main.py
@@ -4,13 +4,14 @@ import json
 import time
 from typing import Any, List
 from llama_index.core.base.base_retriever import BaseRetriever
+from numpy import equal
 from opik import Dataset, Opik, track, DatasetItem
 from llama_index.core.base.llms.types import CompletionResponse
 from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
 from llama_index.core.schema import Document
 from llama_index.core.settings import Settings
 from llama_index.embeddings.ollama import OllamaEmbedding
-from opik.evaluation import evaluate
+from opik.evaluation import evaluate, types
 from opik.evaluation.evaluation_result import EvaluationResult
 from opik.evaluation.metrics import Equals, Hallucination
 from opik.integrations.openai import track_openai
@@ -29,28 +30,19 @@ Settings.embed_model = embeddings
 llm = Ollama(model=QUERY_MODEL, request_timeout=600.0,
              base_url="http://localhost:11434", additional_kwargs={"max_length": 512})
 # %%
-
-
 @track
 def your_llm_application(input: str) -> str:
     response: CompletionResponse = llm.complete(input)
 
     return response.text
-# %%
-# Define the evaluation task
 
 
-def evaluation_task(x: DatasetItem) -> dict[str, Any]:
-    return {
-        "input": x.input['user_question'],
-        "output": your_llm_application(x.input['user_question']),
-        "context": your_context_retriever(x.input['user_question'])
-    }
+
 # %%
 
 
 documents: List[Document] = SimpleDirectoryReader(
-    f"../data/{DATASET}").load_data()   
+    f"../data/{DATASET}").load_data()
 index: VectorStoreIndex = VectorStoreIndex.from_documents(
     documents, show_progress=True)
 retriever: BaseRetriever = index.as_retriever()
@@ -59,8 +51,18 @@ retriever: BaseRetriever = index.as_retriever()
 @track
 def your_context_retriever(input: str) -> List:
 
-    return retriever.retrieve(input)
+    return ["..."]#retriever.retrieve(input)
 
+
+# %%
+# Define the evaluation task
+def evaluation_task(x: DatasetItem)  -> dict[str, str]:
+    
+    return {
+        "input": x.input['user_question'],
+        "output": your_llm_application(x.input['user_question']),
+        "context": your_context_retriever(x.input['user_question'])
+    }
 
 # %%
 # Create a simple dataset
@@ -87,19 +89,18 @@ except:
 # %%
 # Define the metrics
 hallucination_metric = Hallucination()
-
+equals_metric = Equals()
 # %%
-start_time: float = time.time()
 evaluation: EvaluationResult = evaluate(
+    task_threads=1,
     experiment_name="My 1st experiment",
     dataset=dataset,
     task=evaluation_task,
-    scoring_metrics=[hallucination_metric],
+    verbose=1,
+    scoring_metrics=[equals_metric,hallucination_metric],
     experiment_config={
         "model": QUERY_MODEL
     }
 )
-end_time: float = time.time()
-print("Time taken  for evaluation: ", end_time - start_time)
 
 # %%
